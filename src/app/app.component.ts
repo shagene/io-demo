263853c1c68a385c3409785f35ee41dd9e09b7e0
shagene <steven.hagene@gmail.com>
2024-02-01 14:42:56 -0500

    change background on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-// app.component.ts
-
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AppBarComponent } from './components/app-bar/app-bar.component';
 import { ThemeService } from './services/theme.service';
@@ -12,44 +10,32 @@ import { NavRailComponent } from './components/nav-rail/nav-rail.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppBarComponent, ThemeSwitcherComponent, NavRailComponent, CommonModule, RouterOutlet, ],
+  imports: [AppBarComponent, ThemeSwitcherComponent, NavRailComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'App';
   currentTheme: string | undefined;
   themeSubscription: Subscription | undefined;
 
   backgroundImage1 = '../assets/images/space2.jpg';
   backgroundImage2 = '../assets/images/space.gif';
-  currentBackgroundImage: string = this.backgroundImage1;
+  currentBackgroundImage: string;
 
-  constructor(private themeService: ThemeService) { }
+  constructor(private themeService: ThemeService) {
+    // Randomly select one of the background images on component initialization
+    this.currentBackgroundImage = this.getRandomBackgroundImage();
+  }
 
   ngOnInit() {
     this.themeSubscription = this.themeService.themeChanged$.subscribe((theme) => {
       this.currentTheme = theme;
     });
-
-    this.toggleBackgroundImage();
   }
 
   toggleTheme() {
     this.themeService.toggleTheme();
-  }
-
-  toggleBackgroundImage() {
-    console.log('Toggling background image...');
-  
-    setTimeout(() => {
-      this.currentBackgroundImage =
-        this.currentBackgroundImage === this.backgroundImage1
-          ? this.backgroundImage2
-          : this.backgroundImage1;
-  
-      this.toggleBackgroundImage(); // Call it again to continue toggling
-    }, 5000); // Change background every 5 seconds (adjust as needed)
   }
 
   ngOnDestroy() {
@@ -58,5 +44,11 @@ export class AppComponent {
     }
   }
 
-  // ngOnDestroy method removed
+  private getRandomBackgroundImage(): string {
+    // Generate a random number (0 or 1) to select one of the images
+    const randomIndex = Math.floor(Math.random() * 2);
+
+    // Return the selected image URL based on the random number
+    return randomIndex === 0 ? this.backgroundImage1 : this.backgroundImage2;
+  }
 }
